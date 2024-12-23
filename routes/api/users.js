bae9bfa3dddae4959618a46ba0be85386a40466a
Ikromjon <57859849+Ikromjon1998@users.noreply.github.com>
2024-12-23 02:51:10 -0500

    completed users route

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,6 +5,11 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
+const passport = require('passport');
+
+// Load Input Validation
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 // Load User model
 const User = require('../../models/User');
@@ -20,12 +25,19 @@ router.get('/test', (req, res) => res.json({
 // @desc    Register a user
 // @access  Public
 router.post('/register', (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
     // Find a user by email
     User
         .findOne({email: req.body.email})
         .then(user => {
             if (user) {
-                return res.status(400).json({ email: 'Email already exists' });
+                errors.email = 'Email already exists';
+                return res.status(400).json(errors);
             } else {
                 const avatar = gravatar.url(req.body.email, {
                     s: '200', // Size
@@ -59,6 +71,12 @@ router.post('/register', (req, res) => {
 // @desc    Login User / Returning JWT Token
 // @access  Public
 router.post('/login', (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
     const email = req.body.email;
     const password = req.body.password;
 
@@ -95,6 +113,17 @@ router.post('/login', (req, res) => {
                     }
                 });
         });
+});
+
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+    res.json({
+        id: req.user.id,
+        name: req.user.name,
+        email: req.user.email
+    });
 });
 
 module.exports = router;
